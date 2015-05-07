f409e13178826b16b6fefe059241ce040460ed86
Alexander Hsu <hsu10405@gmail.com>
2015-05-07 11:22:28 -0500

    Tidy the code

--- Component Test Codes/voltageReg_test/voltageReg_test.ino
+++ Component Test Codes/voltageReg_test/voltageReg_test.ino
@@ -4,31 +4,29 @@
 #define int2vin 0.019531
 #define int2vout 0.00488281
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(voltPin, OUTPUT);
-  pinMode(voutPin, INPUT);
-  Serial.begin(9600);
+    pinMode(voltPin, OUTPUT);
+    pinMode(voutPin, INPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-  int intensity = 0;
-  // put your main code here, to run repeatedly:
-  for(intensity; intensity <= maxVolt; intensity++){
-    analogWrite(voltPin, intensity);
-    delay(50);
-    Serial.print("Currnet Vin is: ");
-    Serial.print(float(intensity)*int2vin);
-    Serial.print(" V");
-    if(float(intensity)*int2vin <= 1.65){
-      Serial.println(" (OFF)");
+    int intensity = 0;
+    for(intensity; intensity <= maxVolt; intensity++){
+        analogWrite(voltPin, intensity);
+        delay(50);
+        Serial.print("Currnet Vin is: ");
+        Serial.print(float(intensity)*int2vin);
+        Serial.print(" V");
+        if(float(intensity)*int2vin <= 1.65){
+            Serial.println(" (OFF)");
+        }
+        else{
+            Serial.println(" (ON)");
+        }
+        Serial.print("Current Vout is: ");
+        Serial.print(float(analogRead(voutPin)*int2vout));
+        Serial.println(" V");
+        Serial.println("");
+        delay(1000);
     }
-    else{
-      Serial.println(" (ON)");
-    }
-    Serial.print("Current Vout is: ");
-    Serial.print(float(analogRead(voutPin)*int2vout));
-    Serial.println(" V");
-    Serial.println("");
-    
-  }
 }
